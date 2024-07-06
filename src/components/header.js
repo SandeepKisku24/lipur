b1e94d17ccf7854fd625d263e137a0d10828177b
SandeepKisku24 <sanjoshkisku24@gmail.com>
2024-07-06 16:17:27 +0530

    changes in background

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,26 @@
 import { Group, Text, Image, Anchor, Stack } from '@mantine/core';
-import React from 'react';
+import React, { useState } from 'react';
 import img from '../Gallery/musical instrument.png';
 import { Link } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 
 const Header = () => {
+
+    const [menu,setMenu] =  useState("");
+
+
     return (
-        <Stack bg="green" style={{ borderRadius: '5px',margin:"10px 10px 0 10px", gap: '20px', padding: '10px 50px', width: '95%', height: '150px' }}  align="stretch">
-            <Group>
+        <Stack bg="#282828" style={{ borderRadius: '5px',margin:"10px 10px 0 10px", gap: '20px', padding: '10px 50px', width: '95%', height: '150px' }}  align="stretch">
+            <Group onMouseOver={()=>{setMenu("home")}}>
                 <img src={img} style={{height:"50px",width:"50px", backgroundColor:"#fff",borderRadius:"50%"}}/>
                 <Text fz={20} fw={700} c="#fff">Lipur</Text>
             </Group>
-            <Link to="/" style={{ textDecoration: 'none', marginLeft:"10px" }}>
-                <Group style={{ display: 'flex', alignItems: 'center' }}>
-                    <HomeIcon style={{ fontSize: '30px', color: '#fff' }} />
-                    <Anchor c="#fff" underline="never" style={{ marginLeft: '10px' }} fz={16}>Home</Anchor>
+            <Link to="/" style={{ textDecoration: 'none', marginLeft:"10px"}}>
+                <Group style={{ display: 'flex', alignItems: 'center', }} m="auto" onMouseOver={()=>{setMenu("home")}} onMouseLeave={()=>{
+                    setMenu("");
+                }}>
+                    <HomeIcon style={{ fontSize:menu=="home"?"30px":"28px",  color: "#fff"  }} />
+                    <Anchor c="#fff" underline="never" style={{ marginLeft: '10px',textDecoration:"none" ,  fontWeight: menu=="home"?700:500 }} fz={16}>Home</Anchor>
                 </Group>
             </Link>
         </Stack>
--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ import AllTimeHits from "./components/recentHit";
 
 const Home = ()=>{
     return(
-        <div style={{width:"73%"}}>
+        <div style={{width:"73%", backgroundColor:"#282828", paddingLeft:"1%", borderRadius:"5px", marginTop:"10px"}}> 
             <PChannels/>
             <AllTimeHits/>
         </div>
